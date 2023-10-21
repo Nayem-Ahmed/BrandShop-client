9718424fbd81d1df2b79c15e157fd39bb6df0294
unknown <freelancernayem.me@gmail.comm>
2023-10-21 23:19:01 +0600

    reponsive solve

--- src/Components/Dontmiss.jsx
+++ src/Components/Dontmiss.jsx
@@ -6,10 +6,15 @@ const Dontmiss = () => {
             <div className="bg-red-500 p-6 text-white text-center rounded-md">
                 <h2 className="font-bold">Don't Miss Out on
                     Tasty Grocery Deals!</h2>
-                    <h3 className="mb-6">Sign Up for the Daily Newsletter</h3>
-                <div className="join">
-                    <input className="input input-bordered join-item " placeholder="Email" />                 
-                    <button className="btn bg-pink-500 join-item rounded-r-full">Subscribe</button>
+                <h3 className="mb-6">Sign Up for the Daily Newsletter</h3>
+                <div className="join flex flex-col items-center sm:flex-row sm:justify-center">
+                    <input
+                        className="input input-bordered join-item mb-2 sm:mb-0 sm:mr-2"
+                        placeholder="Email"
+                    />
+                    <button className="btn bg-pink-500 join-item rounded-r-full">
+                        Subscribe
+                    </button>
                 </div>
 
             </div>
